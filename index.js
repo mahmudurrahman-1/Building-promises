4a25227cdcea5d1c6a44cab83d852ca4bd87776c
mahmudurrahman-1 <rthe1761@gmail.com>
2022-05-15 16:07:49 +0600

    call back hell and using promise instead

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+//npm dependency for http get request
+const superagent = require("superagent");
+//use callback hell by using promises
+//a promise implements the concept of a future value. It means a value we are expecting to recieve
+//we will wait for it to come back with the data
+fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
+  //end method is actually used to get data. it accepts two parameter-> err,response
+  superagent
+    .get(
+      `https://dog.ceo/api/breed/${data}/images
+`
+    )
+    .then((res) => {
+      fs.writeFile(`doggy.txt`, `${res.body.message}`, (err) => {
+        console.log(`saved`);
+      });
+    })
+    .catch((err) => {
+      console.log("there is an error");
+    });
+});
---
+++ promises.js
@@ -0,0 +1,40 @@
+//here we are gonna use Promise instead of callback function
+
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilepro = (file) => {
+  //promise constructor of  es6--it takes in executor function contains two arguments
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      resolve(data);
+    });
+  });
+};
+
+//
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      console.log(`I hope err is ${err}`);
+      resolve("success");
+    });
+  });
+};
+
+readFilepro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    return superagent.get(
+      `https://dog.ceo/api/breed/${data}/images
+`
+    );
+  })
+  .then((data) => {
+    writeFilePro(`sdi.txt`, `${data.body.message}`);
+  })
+  .then(() => {
+    console.log("The file is saved successfully");
+  })
+  .catch((err) => {
+    console.log("no error ");
+  });
